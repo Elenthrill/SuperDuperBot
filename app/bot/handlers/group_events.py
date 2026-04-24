7369f91fad41f6b0eede86b3f7c7d3df234bd781
Elenthrill <elenthrill@gmail.com>
2026-04-24 20:17:22 +0300

    исправил баг 6,9, + подправил лшибку при my_group без групп

--- app/bot/handlers/group_events.py
+++ app/bot/handlers/group_events.py
@@ -4,6 +4,8 @@ from aiogram import Router
 from aiogram.types import ChatMemberUpdated
 from aiogram.filters import ChatMemberUpdatedFilter
 from aiogram.filters.chat_member_updated import KICKED, LEFT
+from app.infastructure.database.db import delete_group
+from psycopg import AsyncConnection
 
 logger = logging.getLogger(__name__)
 
@@ -13,7 +15,7 @@ group_events_router = Router()
 @group_events_router.my_chat_member(
     ChatMemberUpdatedFilter(member_status_changed=KICKED | LEFT)
 )
-async def bot_removed_from_group(event: ChatMemberUpdated):
+async def bot_removed_from_group(event: ChatMemberUpdated, conn: AsyncConnection):
     """
     Срабатывает, когда бота удалили из группы / супер-группы
     """
@@ -24,13 +26,9 @@ async def bot_removed_from_group(event: ChatMemberUpdated):
     chat_id = event.chat.id
     chat_title = event.chat.title
 
-    logger.warning(
-        "Бот был удалён из группы: %s (%s)",
-        chat_title,
-        chat_id
-    )
+    logger.warning("Бот был удалён из группы: %s (%s)", chat_title, chat_id)
 
     # Переменная для сохранения в бд
     removed_group_id = chat_id
-
+    await delete_group(conn, group_id=removed_group_id)
     print(f"DELETE GROUP FROM DB: {removed_group_id}")
